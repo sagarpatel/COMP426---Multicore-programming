5478c982ef528cf7eda78579ab099a2e1a90a03c
unknown <Sagar@.(none)>
2012-03-03 02:16:07 +0000

    Tons of changes made, too long too list, should have commited eariler. Currently testing vec_splats

--- N_Body-PPU-Only/NBodyPPUOnly.c
+++ N_Body-PPU-Only/NBodyPPUOnly.c
@@ -4,19 +4,20 @@
 #include <time.h>
 #include <altivec.h>
 #include <ppu_intrinsics.h>
+//#include <vec_types.h>
 
 
 
-/*
-int a[4] __attribute__((aligned(16))) = {1,3,5,7};
-int b[4] __attribute__((aligned(16))) = {2,3,6,8};
-int c[4] __attribute__((aligned(16)));
-*/
+float a[4] __attribute__((aligned(16))) = {1,2,3,4};
+float b[4] __attribute__((aligned(16))) = {0,1,0,0};
+float c[4] __attribute__((aligned(16)));
+
 
 #define PARTICLES_MAXCOUNT 128 //must be power of 2 in orderfor array data align to work later on
 #define PARTICLES_DEFAULTMASS 1.0
 #define GRAVITATIONALCONSTANT 1.0
 #define DELTA_TIME 1.0;
+#define GRID_SIZE 1000;
 
 typedef struct 
 {
@@ -28,12 +29,54 @@ particle_Data;
 particle_Data particle_Array[PARTICLES_MAXCOUNT] __attribute__((aligned(sizeof(particle_Data)*PARTICLES_MAXCOUNT)));
 
 
+__vector unsigned char yzxwMask = { 4,5,6,7, 8,9,10,11, 0,1,2,3,  12,13,14,15};
+__vector unsigned char zxywMask = { 8,9,10,11, 0,1,2,3, 4,5,6,7,  12,13,14,15};
+
+
+
 int main(int argc, char **argv)
 {
 
 
-///main loop
+// setup, assign particles initla positions and masses
+// this is done in scalar fashion, NOT SIMD
+// insignificant to performance since it's only done once
 
+	//seed random generator
+	srand( time(NULL) );
+
+	printf("Printing out particles and their randomly assigned positions: \n\n");
+
+	int pC = 0;
+	for(pC = 0; pC < PARTICLES_MAXCOUNT; ++pC)
+	{
+		int grideSize = GRID_SIZE;
+
+		float xPos = (float)( rand() % grideSize  - grideSize/2);
+		float yPos = (float)( rand() % grideSize  - grideSize/2);
+		float zPos = (float)( rand() % grideSize  - grideSize/2);
+
+		particle_Array[pC].position[0] = xPos;
+		particle_Array[pC].position[0] = yPos;
+		particle_Array[pC].position[0] = zPos;
+
+
+		particle_Array[pC].position = vec_splats(xPos);
+
+		particle_Array[pC].velocity[3] = PARTICLES_DEFAULTMASS;
+
+		printf("Particle %d:   ", pC );
+		printf("x= %f, y=%f, z=%f", xPos, yPos, zPos);
+		printf("\n");
+	}
+
+
+
+
+
+
+
+///main loop
 	
 
 	// temp particle Datas used for calculations, not pointers, purposefully passed by value
@@ -44,7 +87,17 @@ int main(int argc, char **argv)
 	//temp vectors used for calculations in loop
 	__vector float tempAcceleration = {0,0,0,0};
 	__vector float tempVelocity = {0,0,0,0};
-	__vector float tempRadius = {0,0,0,0};
+	__vector float tempDistance = {0,0,0,0}; //--> use 4th element to store radius
+	__vector float tempDistanceRL1 = {0,0,0,0};
+	__vector float tempDistanceRL2 = {0,0,0,0};
+
+	__vector float tempNumerator = {0,0,0,0};
+	__vector float tempMassSplat = {0,0,0,0};
+	__vector float tempGConstant = {GRAVITATIONALCONSTANT,GRAVITATIONALCONSTANT,GRAVITATIONALCONSTANT,GRAVITATIONALCONSTANT };
+
+	__vector float zeroVector = {0,0,0,0};
+
+
 
 	//stupid C99, need to declare indicies before for loops
 	int i = 0;
@@ -52,10 +105,8 @@ int main(int argc, char **argv)
 
 	for(i = 0; i<PARTICLES_MAXCOUNT; ++i)
 	{
-
 		//cache the particle data struct to the temp declared outside the loops
 		pDi = particle_Array[i];
-
 
 		for(j = 0; j<PARTICLES_MAXCOUNT; ++j)
 		{
@@ -66,11 +117,34 @@ int main(int argc, char **argv)
 
 			//cache the particle data struct to the temp declared outside the loops
 			pDj = particle_Array[j];
+
+			tempDistance = vec_sub(pDj.position,pDi.position); //actual distance vector between objects i and j
+			//use the distance vector  right now for numerator, before we overwrite is later in the code
+			tempMassSplat = vec_splats(pDj.velocity[3]);
+
+			tempDistance = vec_madd(tempDistance,tempDistance,zeroVector); //square the vector
+
+			 //Assembly for vector rotate
+			//__asm__("addi    4,4,1;");
+
+
+			//using perm instead of rotate, bleurg
+			tempDistanceRL1 = vec_perm(tempDistance, zeroVector, yzxwMask); // imitates lxfloat left rotate
+			tempDistanceRL2 = vec_perm(tempDistance, zeroVector, zxywMask); // imitates 2xfloat left rotate
+
+			//add both
+			tempDistanceRL1 = vec_add(tempDistanceRL1, tempDistanceRL2);
+			//add to original to get total ---> x+y+z
+			tempDistance = vec_add(tempDistance, tempDistanceRL1);
+			//tempDistance is now total distance squared
+
+			//now need to cube tempDistance
+			tempDistance = vec_madd(tempDistance, tempDistance, zeroVector); //squared
+			tempDistance = vec_madd(tempDistance, tempDistance, zeroVector); //cubbed
+
+			//get inverse square root
+			tempDistance = vec_rsqrte(tempDistance);
 			
-
-
-
-
 
 		}
 
@@ -81,18 +155,20 @@ int main(int argc, char **argv)
 
 
 
-
-
-
 /*
-  __vector signed int *va = (__vector signed int *) a;
-  __vector signed int *vb = (__vector signed int *) b;
-  __vector signed int *vc = (__vector signed int *) c;
+  __vector  float *va = (__vector  float *) a;
+  __vector  float *vb = (__vector  float *) b;
+  __vector  float *vc = (__vector  float *) c;
 
-  *vc = vec_add(*va,*vb);
+  //*vc = vec_sub(*va,*vb);
 
-printf("c[0]=%d, c[1]=%d, c[2]=%d, c[3]=%d¥n", c[0], c[1], c[2], c[3]);
+
+  *vc = vec_perm(*va,*vb, yzxwMask);
+
+printf("c[0]=%f, c[1]=%f, c[2]=%f, c[3]=%f¥n", c[0], c[1], c[2], c[3]);
 */
+
+
 return 0;
 
 
